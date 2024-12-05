0dc7c952b08ab5662c20693306e4e2d2903b41ff
Shanzid Helal Emon <142948665+shanzidemon@users.noreply.github.com>
2024-12-05 22:04:13 +0600

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,8 @@
+def display_menu():
+    print("\n--- Contact Book Menu ---")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contacts")
+    print("4. Remove Contact")
+    print("5. Exit")
+    return input("Enter your choice: "
